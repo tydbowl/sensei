a4aaa8e7cce96c29e202237861a296544b561bce
Jonathan Milgrom <jmilgrom@triplelift.com>
2015-08-08 11:07:34 -0400

    defaulting to login page

--- www/js/AdvertiserCtrl.js
+++ www/js/AdvertiserCtrl.js
@@ -1,5 +1,6 @@
 angular.module('starter.controllers.advertiser', [])
 
 .controller('AdvertiserCtrl', function($scope, $stateParams){
+  var TACTIC_URL = 'native_advertising/buy_side/tactic';
   $scope.advertiserId = $stateParams.advertiserId;
 });
--- www/js/app.js
+++ www/js/app.js
@@ -110,6 +110,6 @@ angular.module('starter', [
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
